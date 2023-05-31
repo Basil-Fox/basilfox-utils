3c2487b7bbca932609ceb994769b2b623a1c09f2
Ritesh Kumar Yadav <siltaz@proton.me>
2023-05-31 10:07:34 +0530

    v0.1.0

--- middleware/header.go
+++ middleware/header.go
@@ -17,23 +17,25 @@ func ValidateHeaders(endpointType string) func(c *fiber.Ctx) error {
 		if endpointType == constant.EndpointPrivate || endpointType == constant.EndpointRefresh {
 			// Check UserID Header
 			if c.Get(constant.HeaderUserId) == "" {
-				return response.SendError(c, fiber.ErrBadGateway)
+				return response.SendError(c, fiber.ErrUnauthorized)
 			}
 
 			// Check TokenID Header
 			if c.Get(constant.HeaderTokenId) == "" {
-				return response.SendError(c, fiber.ErrBadGateway)
+				return response.SendError(c, fiber.ErrUnauthorized)
 			}
 
 			// Check TokenKind Header
 			if c.Get(constant.HeaderTokenKind) == "" {
-				return response.SendError(c, fiber.ErrBadGateway)
+				return response.SendError(c, fiber.ErrUnauthorized)
 			}
 
+			// Check if AccessToken is used for authentication
 			if endpointType == constant.EndpointPrivate && c.Get(constant.HeaderTokenKind) != constant.TokenTypeAccess {
 				return response.SendError(c, fiber.ErrUnauthorized)
 			}
 
+			// Check if RefreshToken is used for authentication
 			if endpointType == constant.EndpointRefresh && c.Get(constant.HeaderTokenKind) != constant.TokenTypeRefresh {
 				return response.SendError(c, fiber.ErrUnauthorized)
 			}
