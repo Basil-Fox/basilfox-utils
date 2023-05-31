017d5eaf94d57610967148a00d39887ba3b52bbb
Ritesh Kumar Yadav <siltaz@proton.me>
2023-05-31 09:53:54 +0530

    v0.0.9

--- constant/constant.go
+++ constant/constant.go
@@ -4,8 +4,16 @@ const (
 	ContextJwtToken  = "jwt_token"
 	ContextRequestId = "request_id"
 
+	EndpointPublic  = "Public"
+	EndpointPrivate = "Private"
+	EndpointRefresh = "Refresh"
+
 	HeaderNamespace = "X-App-Namespace"
 	HeaderRequestId = "X-Request-ID"
 	HeaderTokenId   = "X-Token-ID"
+	HeaderTokenKind = "X-Token-Kind"
 	HeaderUserId    = "X-User-ID"
+
+	TokenTypeAccess  = "access"
+	TokenTypeRefresh = "refresh"
 )
--- middleware/header.go
+++ middleware/header.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func ValidateHeaders(isUserRequired bool) func(c *fiber.Ctx) error {
+func ValidateHeaders(endpointType string) func(c *fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
 
 		// Check Namespace Header
@@ -14,14 +14,29 @@ func ValidateHeaders(isUserRequired bool) func(c *fiber.Ctx) error {
 			return response.SendError(c, fiber.ErrBadGateway)
 		}
 
-		// Check UserID Header
-		if c.Get(constant.HeaderUserId) == "" && isUserRequired {
-			return response.SendError(c, fiber.ErrBadGateway)
-		}
+		if endpointType == constant.EndpointPrivate || endpointType == constant.EndpointRefresh {
+			// Check UserID Header
+			if c.Get(constant.HeaderUserId) == "" {
+				return response.SendError(c, fiber.ErrBadGateway)
+			}
 
-		// Check TokenID Header
-		if c.Get(constant.HeaderTokenId) == "" && isUserRequired {
-			return response.SendError(c, fiber.ErrBadGateway)
+			// Check TokenID Header
+			if c.Get(constant.HeaderTokenId) == "" {
+				return response.SendError(c, fiber.ErrBadGateway)
+			}
+
+			// Check TokenKind Header
+			if c.Get(constant.HeaderTokenKind) == "" {
+				return response.SendError(c, fiber.ErrBadGateway)
+			}
+
+			if endpointType == constant.EndpointPrivate && c.Get(constant.HeaderTokenKind) != constant.TokenTypeAccess {
+				return response.SendError(c, fiber.ErrUnauthorized)
+			}
+
+			if endpointType == constant.EndpointRefresh && c.Get(constant.HeaderTokenKind) != constant.TokenTypeRefresh {
+				return response.SendError(c, fiber.ErrUnauthorized)
+			}
 		}
 
 		return c.Next()
