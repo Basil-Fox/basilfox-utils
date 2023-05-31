e64738e34e0c30b484cec2157846c9cdbaeee23f
Ritesh Kumar Yadav <siltaz@proton.me>
2023-05-31 08:47:44 +0530

    new constants added

--- constant/constant.go
+++ constant/constant.go
@@ -1,9 +1,11 @@
 package constant
 
 const (
-	HeaderRequestId = "X-Request-ID"
-	HeaderUserId    = "X-User-ID"
-	HeaderNamespace = "X-App-Namespace"
-
+	ContextJwtToken  = "jwt_token"
 	ContextRequestId = "request_id"
+
+	HeaderNamespace = "X-App-Namespace"
+	HeaderRequestId = "X-Request-ID"
+	HeaderTokenId   = "X-Token-ID"
+	HeaderUserId    = "X-User-ID"
 )
