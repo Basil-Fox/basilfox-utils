9dc1ea8ae0c82fb4a80201f172b4bf7b85ddb0d3
Ritesh Kumar Yadav <siltaz@proton.me>
2023-06-01 12:49:12 +0530

    v0.1.2

---
+++ kafka/producer.go
@@ -0,0 +1,54 @@
+package kafka
+
+import (
+	"log"
+
+	"github.com/Shopify/sarama"
+)
+
+// Consumer
+func CreateConsumer(brokersUrl []string) (sarama.Consumer, error) {
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+
+	consumer, err := sarama.NewConsumer(brokersUrl, config)
+	if err != nil {
+		return nil, err
+	}
+	return consumer, nil
+}
+
+// Producer
+func createProducer(brokersUrl []string) (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+
+	producer, err := sarama.NewSyncProducer(brokersUrl, config)
+	if err != nil {
+		return nil, err
+	}
+	return producer, nil
+}
+
+// Publisher
+func PublishMessage(kafkaBroker string, topic string, message []byte) error {
+	brokersUrl := []string{kafkaBroker}
+	producer, err := createProducer(brokersUrl)
+	if err != nil {
+		return err
+	}
+	defer producer.Close()
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+	log.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", topic, partition, offset)
+	return nil
+}
