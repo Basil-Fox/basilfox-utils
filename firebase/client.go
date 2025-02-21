b7094bffe12c2ce6b96537170ca3fcc71871ed4c
Ritesh Kumar Yadav <ritesh.yadav@adder.com>
2025-02-21 15:33:43 +0000

    version up

--- firebase/client.go
+++ firebase/client.go
@@ -9,8 +9,19 @@ import (
 
 var App *firebase.App
 
-func InitApp(credentialsPath string) error {
+func InitWithFile(credentialsPath string) error {
 	opt := option.WithCredentialsFile(credentialsPath)
+	_app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		return err
+	}
+
+	App = _app
+	return nil
+}
+
+func InitWithJson(credentialsJSON []byte) error {
+	opt := option.WithCredentialsJSON(credentialsJSON)
 	_app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		return err
