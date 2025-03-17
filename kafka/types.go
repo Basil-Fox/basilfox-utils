8b33afa291fea06d50729008309b4613be4d1b9f
Ritesh Kumar Yadav <ritesh.yadav@adder.com>
2025-03-17 14:34:23 +0000

    kafka msg improvement

--- kafka/types.go
+++ kafka/types.go
@@ -18,18 +18,25 @@ type ConsumerMessage = sarama.ConsumerMessage
 
 type KafkaWorker func(*ConsumerMessage, *zerolog.Logger) error
 
+type KafkaMessage struct {
+	RequestID string
+}
+
 type LogoutMessage struct {
+	KafkaMessage
 	TokenID  string
 	ExpireAt int64
 }
 
 type AccountLinkMessage struct {
+	KafkaMessage
 	UserID    string
 	GuestID   string
 	Namespace string
 }
 
 type SendEmailMessage struct {
+	KafkaMessage
 	Recipient string
 	Subject   string
 	BodyType  string
@@ -37,6 +44,7 @@ type SendEmailMessage struct {
 }
 
 type SendPushNotificationMessage struct {
+	KafkaMessage
 	Tokens []string
 	Title  string
 	Body   string
